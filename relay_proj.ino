c2fe2f64e80fa94356697da99db02e4b20fe2f90
Victor Hugo Rizzo Moura <43479155+vhRizzo@users.noreply.github.com>
2023-01-24 18:01:50 -0300

    Update relay_proj.ino

--- relay_proj.ino
+++ relay_proj.ino
@@ -1,42 +1,107 @@
-#define QNT_RELE 1
+#include "driver/gpio.h"
 
-uint8_t pin[QNT_RELE] = {35};
-volatile int estadoAnterior[QNT_RELE] = {HIGH};
-volatile unsigned long marcaOff[QNT_RELE] = {0};
-volatile unsigned long marcaOn[QNT_RELE] = {0};
-bool rst = false;
+#define QNT_RELE 2
 
-void IRAM_ATTR itr0()
-{
-  int estadoAtual = digitalRead(pin[0]);
-  if (estadoAtual == estadoAnterior[0]) return;
-  if (estadoAtual == LOW)
-  {
-    marcaOff[0] = millis();
-    estadoAnterior[0] = estadoAtual;
-  } else
-  {
-    marcaOn[0] = millis();
-    estadoAnterior[0] = estadoAtual;    
-  }
-}
+int debounce = 100;
+volatile uint8_t pin[QNT_RELE] = {32, 33};
+volatile int estadoAnterior[QNT_RELE] = {1, 1};
+volatile unsigned long marcaOff[QNT_RELE] = {0, 0};
+volatile unsigned long marcaOn[QNT_RELE] = {0, 0};
+volatile int flag[QNT_RELE] = {0, 0};
+
+void IRAM_ATTR r0_isr (void *arg);
+void IRAM_ATTR r1_isr (void *arg);
+void r0_task (void *pvParameters);
+void r1_task (void *pvParameters);
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(pin[0], INPUT);
-  attachInterrupt(digitalPinToInterrupt(pin[0]), itr0, CHANGE);
+
+  xTaskCreate(r0_task, "Rele0_Task", 1024*4, NULL, configMAX_PRIORITIES - 10, NULL);
+  xTaskCreate(r1_task, "Rele1_Task", 1024*4, NULL, configMAX_PRIORITIES - 10, NULL);
 }
 
-void loop()
+void IRAM_ATTR r0_isr (void *arg)
 {
-  if(rst)
+  uint32_t gpio_estado = gpio_get_level((gpio_num_t)pin[0]);
+  if (gpio_estado == estadoAnterior[0]) return;
+  else if (gpio_estado == 0)
   {
-    Serial.print("Tempo da desativacao: ");
-    Serial.println(marcaOff[0]);
-    Serial.print("Tempo da ativacao: ");
-    Serial.println(marcaOn[0]);
-    Serial.print("Tempo total desligado: ");
-    Serial.println(marcaOn[0] - marcaOff[0]);
+    marcaOff[0] = millis();
+    estadoAnterior[0] = gpio_estado;
+    flag[0] = 1;
+  } else {
+    marcaOn[0] = millis();
+    estadoAnterior[0] = gpio_estado;
+    flag[0] = 2;
   }
 }
+
+void IRAM_ATTR r1_isr (void *arg)
+{
+  uint32_t gpio_estado = gpio_get_level((gpio_num_t)pin[1]);
+  if (gpio_estado == estadoAnterior[1]) return;
+  else if (gpio_estado == 0)
+  {
+    marcaOff[1] = millis();
+    estadoAnterior[1] = gpio_estado;
+    flag[1] = 1;
+  } else {
+    marcaOn[1] = millis();
+    estadoAnterior[1] = gpio_estado;
+    flag[1] = 2;
+  }
+}
+
+void r0_task (void *pvParameters)
+{
+  gpio_config_t io_conf = {
+      .pin_bit_mask = (uint64_t)(1ULL << pin[0]),
+      .mode = GPIO_MODE_INPUT_OUTPUT,
+      .pull_up_en = GPIO_PULLUP_DISABLE,
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,
+      .intr_type = GPIO_INTR_ANYEDGE,
+  };
+  gpio_config(&io_conf);
+  gpio_install_isr_service(0);
+  gpio_isr_handler_add((gpio_num_t)pin[0], r0_isr, NULL);
+  while(1)
+  {
+    if(flag[0] == 1) {
+      Serial.printf("R0 Desativado\n");
+      flag[0] = 0;
+    } else if(flag[0] == 2) {
+      Serial.printf("R0 Ativado\nTempo fora de operacao: %d\n\n", marcaOn[0] - marcaOff[0]);
+      flag[0] = 0;
+    }
+    vTaskDelay(QNT_RELE+2);
+  }
+}
+
+void r1_task (void *pvParameters)
+{
+  gpio_config_t io_conf = {
+      .pin_bit_mask = (uint64_t)(1ULL << pin[1]),
+      .mode = GPIO_MODE_INPUT_OUTPUT,
+      .pull_up_en = GPIO_PULLUP_DISABLE,
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,
+      .intr_type = GPIO_INTR_ANYEDGE,
+  };
+  gpio_config(&io_conf);
+  gpio_install_isr_service(0);
+  gpio_isr_handler_add((gpio_num_t)pin[1], r1_isr, NULL);
+  while(1)
+  {
+    if(flag[1] == 1) {
+      Serial.printf("R1 Desativado\n");
+      flag[1] = 0;
+    } else if(flag[1] == 2) {
+      Serial.printf("R1 Ativado\nTempo fora de operacao: %d\n\n", marcaOn[1] - marcaOff[1]);
+      flag[1] = 0;
+    }
+    vTaskDelay(QNT_RELE+2);
+  }
+}
+
+void loop() {}
